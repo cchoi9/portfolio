a67897734ca9610bcef84b5994f32df4914a824c
Chris Choi <chc5139@gmail.com>
2019-09-10 20:12:08 -0400

    about me completed

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,6 @@ import Button from "react-bootstrap/Button";
 export default function About() {
   return (
     <div id="about" className="About text-black py-4">
-      <h1 className="lead about-text">About Me</h1>
       <div className="container row w-90 mx-auto p-2 rounded about-content">
         <div className="col-12 col-lg-6 text-center">
           <img
@@ -15,6 +14,10 @@ export default function About() {
           />
         </div>
         <div className="col-12 col-lg-6 about-right">
+          <h1 className="display-4 about-text">
+            <u>About Me</u>
+          </h1>
+          <br />
           <p className="lead">
             The human body is no different than a complex machine. Before my
             transition into the tech industry, I formerly worked as a exercise
@@ -23,48 +26,92 @@ export default function About() {
             team of individuals who drive one another to scale and optimize
             projects every step of the way.{" "}
           </p>
-          <div className="row mx-auto">
-            <div className="col-4">
-              <a href="https://github.com/cchoi9" target="_blank">
-                <img
-                  src="https://img.icons8.com/nolan/64/000000/github.png"
-                  className="contact"
-                  aria-hidden="true"
-                />
-              </a>
-            </div>
-            <div className="col-4">
-              <a href="https://www.linkedin.com/in/chrishchoi/" target="_blank">
-                <img
-                  src="https://img.icons8.com/cute-clipart/64/000000/linkedin.png"
-                  className="contact"
-                  aria-hidden="true"
-                />
-              </a>
-            </div>
-            <div className="col-4">
-              <a href="https://gmail.com" target="_blank">
-                <img
-                  src="https://img.icons8.com/color/64/000000/gmail.png"
-                  className="contact"
-                  aria-hidden="true"
-                />
-              </a>
+          <br />
+          <p className="lead mb-1">Front-End</p>
+          <div className="row ml-1">
+            <div className="ml-0">
+              <span className="border border-info px-2 rounded mx-1">
+                <small>React</small>
+              </span>
+              <span className="border border-info px-2 rounded mx-1">
+                <small>React Native</small>
+              </span>
+              <span className="border border-info px-2 rounded mx-1">
+                <small>Redux</small>
+              </span>
+              <span className="border border-info px-2 rounded mx-1">
+                <small>HTML5</small>
+              </span>
+              <span className="border border-info px-2 rounded mx-1">
+                <small>CSS3</small>
+              </span>
             </div>
           </div>
-          <Button
-            variant="light"
-            id="resume-btn"
-            className="mt-5"
-            href={require("../assets/chris-3.png")}
-            download="resume.pdf"
-          >
-            <i className="fa fa-cloud-download" aria-hidden="true"></i>
-            {"   "}
-            <p>Resume</p>
-          </Button>
+          <p className="mt-3 mb-1 lead">Back-End</p>
+          <div className="row ml-1">
+            <div className="mt-0">
+              <span className="border border-info px-2 rounded mx-1">
+                <small>NodeJS</small>
+              </span>
+              <span className="border border-info px-2 rounded mx-1">
+                <small>Express</small>
+              </span>
+              <span className="border border-info px-2 rounded mx-1">
+                <small>PostgreSQL</small>
+              </span>
+              <span className="border border-info px-2 rounded mx-1">
+                <small>Firebase</small>
+              </span>
+              <span className="border border-info px-2 rounded mx-1">
+                <small>Sequelize</small>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
+}
+
+{
+  /* <div className="row mx-auto">
+<div className="col-4">
+  <a href="https://github.com/cchoi9" target="_blank">
+    <img
+      src="https://img.icons8.com/nolan/64/000000/github.png"
+      className="contact"
+      aria-hidden="true"
+    />
+  </a>
+</div>
+<div className="col-4">
+  <a href="https://www.linkedin.com/in/chrishchoi/" target="_blank">
+    <img
+      src="https://img.icons8.com/cute-clipart/64/000000/linkedin.png"
+      className="contact"
+      aria-hidden="true"
+    />
+  </a>
+</div>
+<div className="col-4">
+  <a href="https://gmail.com" target="_blank">
+    <img
+      src="https://img.icons8.com/color/64/000000/gmail.png"
+      className="contact"
+      aria-hidden="true"
+    />
+  </a>
+</div>
+</div>
+<Button
+variant="light"
+id="resume-btn"
+className="mt-5"
+href={require("../assets/chris-3.png")}
+download="resume.pdf"
+>
+<i className="fa fa-cloud-download" aria-hidden="true"></i>
+{"   "}
+<p>Resume</p>
+</Button> */
 }
