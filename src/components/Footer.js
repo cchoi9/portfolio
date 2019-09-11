a43f4ef24d228dacd3afc99e1eb5583197b3e62e
Chris Choi <chc5139@gmail.com>
2019-09-11 13:32:41 -0400

    icon footer size changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className="col-4">
           <a href="https://www.linkedin.com/in/chrishchoi/" target="_blank">
             <img
-              src="https://img.icons8.com/cute-clipart/64/000000/linkedin.png"
+              src="https://img.icons8.com/cute-clipart/96/000000/linkedin.png"
               className="contact"
               aria-hidden="true"
             />
@@ -17,7 +17,7 @@ export default function Footer() {
         <div className="col-4">
           <a href="https://github.com/cchoi9" target="_blank">
             <img
-              src="https://img.icons8.com/nolan/68/000000/github.png"
+              src="https://img.icons8.com/nolan/96/000000/github.png"
               className="contact"
               aria-hidden="true"
             />
@@ -26,7 +26,7 @@ export default function Footer() {
         <div className="col-4">
           <a href="mailto:chc5139@gmail.com">
             <img
-              src="https://img.icons8.com/color/64/000000/gmail.png"
+              src="https://img.icons8.com/color/96/000000/gmail.png"
               className="contact"
               aria-hidden="true"
             />
