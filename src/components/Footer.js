528822110202c638f610d8de6e7f6fc1ae63e674
Chris Choi <chc5139@gmail.com>
2019-09-12 10:55:19 -0400

    fixed footer width

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,19 @@ export default function Footer() {
   return (
     <div className="Footer w-100 mt-5">
       <div className="row m-auto">
-        <div className="col-4">
+        <div className="col-4 w-75">
           <a href="https://www.linkedin.com/in/chrishchoi/" target="_blank">
             <img
-              src="https://img.icons8.com/cute-clipart/96/000000/linkedin.png"
+              src="https://img.icons8.com/cute-clipart/70/000000/linkedin.png"
               className="contact"
               aria-hidden="true"
             />
           </a>
         </div>
-        <div className="col-4">
+        <div className="col-4 w-75">
           <a href="https://github.com/cchoi9" target="_blank">
             <img
-              src="https://img.icons8.com/nolan/96/000000/github.png"
+              src="https://img.icons8.com/nolan/72/000000/github.png"
               className="contact"
               aria-hidden="true"
             />
@@ -26,7 +26,7 @@ export default function Footer() {
         <div className="col-4">
           <a href="mailto:chc5139@gmail.com">
             <img
-              src="https://img.icons8.com/color/96/000000/gmail.png"
+              src="https://img.icons8.com/color/70/000000/gmail.png"
               className="contact"
               aria-hidden="true"
             />
